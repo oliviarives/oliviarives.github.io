d874b5e1e683fcadd79feb68a86b02b623770e6f
Olivia Rivs <155980126+oliviarives@users.noreply.github.com>
2024-03-04 10:12:13 +0100

    Upload web site v2

---
+++ style/ecriture.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const title = document.getElementById('auto-text')
+const text = "Nous sommes des coucoux";
+
+const play = () => {
+    title.innerHTML = text
+} 
+
+play()
